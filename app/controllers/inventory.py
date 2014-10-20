b2759a83c563757fc8f44db707d498ca4188fa83
TenthSpeedWriter <addseale2@gmail.com>
2014-10-20 14:57:31 -0500

    Added Ferris framework and initial work
    Includes an inventory model, the OAuth2 credentials, and a simple
    controller & template viewable at /inventory

---
+++ app/controllers/inventory.py
@@ -0,0 +1,19 @@
+__author__ = 'Adam'
+
+from ferris import Controller, scaffold
+from ferris.components import oauth
+from apiclient.discovery import build
+
+class Inventory(Controller):
+    class Meta:
+        prefixes     = ('viewer','editor',)
+        components   = (scaffold.Scaffolding,oauth.OAuth) # This is how we OAuth2
+        oauth_scopes = ['https://www.googleapis.com/auth/cloud-platform',
+                        'https://www.googleapis.com/auth/datastore',
+                        'https://www.googleapis.com/auth/userinfo.email']
+
+#    @oauth.require_credentials
+#    def list(self):
+#        return "Nonsense from the controller goes here"
+
+    list = scaffold.list
---
+++ app/models/inventory.py
@@ -0,0 +1,26 @@
+__author__ = 'Chef'
+
+from ferris import BasicModel
+from google.appengine.ext import ndb
+
+class Inventory (BasicModel):
+    isActive       = ndb.BooleanProperty()
+    type           = ndb.StringProperty()
+    item           = ndb.StringProperty()
+    description    = ndb.TextProperty()
+    salesTaxCode   = ndb.StringProperty()
+    account        = ndb.StringProperty()
+    cogsAccount    = ndb.StringProperty()
+    assetAccount   = ndb.StringProperty()
+    quantityOnHand = ndb.IntegerProperty()
+    unitOfMeasure  = ndb.StringProperty()
+    cost           = ndb.FloatProperty()
+    price          = ndb.FloatProperty()
+    reorderPoint   = ndb.IntegerProperty()
+    mpn            = ndb.StringProperty()
+    weightPerPiece = ndb.FloatProperty()
+    taxAgency      = ndb.StringProperty()
+    preferredVendor         = ndb.StringProperty()
+    unitOfMeasureSet        = ndb.StringProperty()
+    purchaseDescription     = ndb.TextProperty()
+    accumulatedDepreciation = ndb.FloatProperty()
